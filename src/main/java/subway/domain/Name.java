3105dd486c8b7c01e1a4e0bca8fb8d5a634d4643
ecsimsw <ecsimsw@gmail.com>
2020-12-10 04:16:25 +0900

    feat : 역, 노선 이름을 포장하는 Name 클래스 구현

--- src/main/java/subway/domain/Name.java
+++ src/main/java/subway/domain/Name.java
@@ -1,13 +1,17 @@
 package subway.domain;
 
 public class Name {
-    private String name;
+    private String value;
 
-    public Name(String name){
-        if(name.length() <2 || name.length() >5){
+    public Name(String value){
+        if(value.length() <2 || value.length() >5){
             throw new IllegalArgumentException("잘못된 길이의 이름입니다.");
         }
 
-        this.name = name;
+        this.value = value;
+    }
+
+    public boolean isSame(Name name){
+        return this.value == name.value;
     }
 }
