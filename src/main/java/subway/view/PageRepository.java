330f26ee8ac01042e862ce553db06a1b555d5342
ecsimsw <ecsimsw@gmail.com>
2020-12-10 14:54:55 +0900

    feat : 노선 관리 화면 구현

--- src/main/java/subway/view/PageRepository.java
+++ src/main/java/subway/view/PageRepository.java
@@ -5,11 +5,12 @@ public class PageRepository {
     }
 
     public static final String selectPageMessage = "## 원하는 기능을 선택하세요.\n";
-    
+
     public static final String mainPage;
     public static final String stationPage;
     public static final String sectionPage;
-    
+    public static final String linePage;
+
     static {
         mainPage = "## 메인 화면\n"
                 + "1. 역 관리\n"
@@ -30,6 +31,13 @@ public class PageRepository {
     static {
         sectionPage = "## 노선 관리 화면\n"
                 + "1. 노선 등록\n"
+                + "2. 노선 삭제\n"
+                + "3. 노선 조회\n"
+                + "B. 돌아가기\n";
+    }
+
+    static {
+        linePage = "1. 노선 등록\n"
                 + "2. 노선 삭제\n"
                 + "3. 노선 조회\n"
                 + "B. 돌아가기\n";
